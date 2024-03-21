ebdd0e97088e3cbc9196fce67555d649d891de0a
arhangel2657 <bebey22881@gmail.com>
2024-03-21 22:44:14 +0200

    test commit

--- test_py.py
+++ test_py.py
@@ -3,9 +3,11 @@ file_data = file.read()
 file.close()
 
 out = {}
-def Add_to_out(key, value):
+
+def add_to_out(key, value):
     out[key] = value
-def Culculate_symbol(symbol):
+
+def culculate_symbol(symbol):
     num_of_symbols = 0
     for s in file_data:
         if s.lower() == symbol:
@@ -13,7 +15,7 @@ def Culculate_symbol(symbol):
     return num_of_symbols
 
 # add_to_out("lines", )
-Add_to_out("words", len(file_data.split(" ")))
-Add_to_out("symbols", len(file_data))
+add_to_out("words", len(file_data.split(" ")))
+add_to_out("symbols", len(file_data))
 
 print(out)
