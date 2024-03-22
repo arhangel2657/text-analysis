974cac839a5276165cedd282720d4690da7bd68c
arhangel2657 <bebey22881@gmail.com>
2024-03-22 13:16:41 +0200

    finish

--- text_analysis.py.py
+++ text_analysis.py.py
@@ -13,13 +13,13 @@ def culculate_symbol(symbol):
             num_of_symbols += 1
     return num_of_symbols
 
-symbol_namber =0
-symbol = ""
+num =0
+sym = ""
 for i in file_data:
-    sym = file_data[symbol_namber]
+    sym = file_data[num]
     if sym.isalpha():
-        add_to_out(file_data[symbol_namber].upper(),file_data.count(symbol))
-    symbol_namber += 1
+        add_to_out(file_data[num].upper(),file_data.count(sym))
+    num += 1
 
 add_to_out("lines", len(file_data.split(".")))
 add_to_out("words", len(file_data.split(" ")))
