51f7dc71cd306fbb025cbf7c51efdb85a37cd868
arhangel2657 <bebey22881@gmail.com>
2024-03-22 12:17:34 +0200

    add lines

--- test_py.py
+++ test_py.py
@@ -14,6 +14,16 @@ def culculate_symbol(symbol):
             num_of_symbols += 1
     return num_of_symbols
 
+
+nam = 0
+for i in file_data:
+    if file_data[nam].upper() == file_data[nam].upper():
+
+    if file_data[nam].isalpha():
+        add_to_out(file_data[nam].upper(),)
+    nam += 1
+
+
 add_to_out("lines", len(file_data.split(".")))
 add_to_out("words", len(file_data.split(" ")))
 add_to_out("symbols", len(file_data))
