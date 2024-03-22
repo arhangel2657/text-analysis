c524253660258c4593205de75de000c04c267eb9
arhangel2657 <bebey22881@gmail.com>
2024-03-22 14:32:16 +0200

    finish

--- text_analysis.py.py
+++ text_analysis.py.py
@@ -2,10 +2,13 @@ file = open("text_for_analysis.txt", "r")
 file_data = file.read().lower()
 file.close()
 
-out = {}
+Out = {}
+
 
 def add_to_out(key, value):
-    out[key] = value
+    Out[key] = value
+
+
 def culculate_symbol(symbol):
     num_of_symbols = 0
     for s in file_data:
@@ -13,16 +16,17 @@ def culculate_symbol(symbol):
             num_of_symbols += 1
     return num_of_symbols
 
-num = 0
-sym = ""
+
+Symbol_number = 0
+Symbol = ""
 for i in file_data:
-    sym = file_data[num]
-    if sym.isalpha():
-        add_to_out(file_data[num].upper(),file_data.count(sym))
-    num += 1
+    Symbol = file_data[Symbol_number]
+    if Symbol.isalpha():
+        add_to_out(file_data[Symbol_number].upper(), file_data.count(Symbol))
+    Symbol_number += 1
 
 add_to_out("lines", len(file_data.split(".")))
 add_to_out("words", len(file_data.split()))
 add_to_out("symbols", len(file_data))
 
-print(out)
+print(Out)
